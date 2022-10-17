a34050b7f1ffe006fe4c5167fa52b79fea444212
Olivier Vignaud <ovi@ubiqube.com>
2022-10-17 17:55:04 +0200

    mano-model: Optimize vnf live instance.

--- src/main/java/com/ubiqube/etsi/mano/dao/mano/VnfLiveInstance.java
+++ src/main/java/com/ubiqube/etsi/mano/dao/mano/VnfLiveInstance.java
@@ -22,6 +22,7 @@ import javax.persistence.Column;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.EntityListeners;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -45,15 +46,15 @@ public class VnfLiveInstance implements BaseEntity, Auditable {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private UUID id;
 
-	@ManyToOne
+	@ManyToOne(fetch = FetchType.LAZY)
 	private VnfInstance vnfInstance;
 
 	private String instantiationLevel;
 
-	@ManyToOne
+	@ManyToOne(fetch = FetchType.LAZY)
 	private VnfTask task;
 
-	@ManyToOne
+	@ManyToOne(fetch = FetchType.LAZY)
 	private VnfBlueprint blueprint;
 
 	/**
