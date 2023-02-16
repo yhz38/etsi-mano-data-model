27fb640fad938482a4334314102092d44cefdcca
Olivier Vignaud <ovi@ubiqube.com>
2023-02-16 09:55:44 +0100

    mano-model: Make more room for attributes condition and action in triiger definitions.

--- src/main/java/com/ubiqube/etsi/mano/dao/mano/TriggerDefinition.java
+++ src/main/java/com/ubiqube/etsi/mano/dao/mano/TriggerDefinition.java
@@ -20,14 +20,14 @@ import java.io.Serializable;
 import java.util.List;
 import java.util.UUID;
 
+import org.hibernate.search.mapper.pojo.mapping.definition.annotation.DocumentId;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-
-import org.hibernate.search.mapper.pojo.mapping.definition.annotation.DocumentId;
-
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -49,8 +49,10 @@ public class TriggerDefinition implements Serializable {
 	@DocumentId
 	private UUID id;
 
+	@Column(length = 5000)
 	private String condition;
 
+	@Column(length = 5000)
 	private String action;
 
 	@ElementCollection(targetClass = String.class)
