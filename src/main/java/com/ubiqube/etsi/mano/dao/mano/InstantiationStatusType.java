9a417da2095715b5a1cf1093ff0def18a5195309
Olivier Vignaud <ovi@ubiqube.com>
2023-02-27 16:34:01 +0100

    License update.

--- src/main/java/com/ubiqube/etsi/mano/dao/mano/InstantiationStatusType.java
+++ src/main/java/com/ubiqube/etsi/mano/dao/mano/InstantiationStatusType.java
@@ -1,5 +1,5 @@
 /**
- *     Copyright (C) 2019-2020 Ubiqube.
+ *     Copyright (C) 2019-2023 Ubiqube.
  *
  *     This program is free software: you can redistribute it and/or modify
  *     it under the terms of the GNU General Public License as published by
