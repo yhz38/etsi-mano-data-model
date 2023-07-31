1233f4df42cc0bf411e3f55b11eb806af2ffc09a
Olivier Vignaud <ovi@ubiqube.com>
2023-07-31 15:25:02 +0200

    Add test for schema on H2 and postgresql.

---
+++ src/test/java/com/ubiqube/etsi/mano/dao/SpringH2Test.java
@@ -0,0 +1,46 @@
+/**
+ *     Copyright (C) 2019-2023 Ubiqube.
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see https://www.gnu.org/licenses/.
+ */
+package com.ubiqube.etsi.mano.dao;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+import javax.sql.DataSource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@EntityScan({ "com.ubiqube.etsi.mano" })
+class SpringH2Test {
+
+	@Autowired
+	DataSource ds;
+
+	@Test
+	void testH2Schema() throws Exception {
+		assertTrue(true);
+		try (Connection conn = ds.getConnection();
+				final PreparedStatement stmt = conn.prepareStatement("select * from \"vnf_package\"", 0)) {
+			stmt.execute();
+		}
+	}
+}
---
+++ src/test/java/com/ubiqube/etsi/mano/dao/SpringMariaDbTest.java
@@ -0,0 +1,46 @@
+/**
+ *     Copyright (C) 2019-2023 Ubiqube.
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see https://www.gnu.org/licenses/.
+ */
+package com.ubiqube.etsi.mano.dao;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.test.context.ActiveProfiles;
+
+//@SpringBootTest
+@EntityScan({ "com.ubiqube.etsi.mano" })
+@ActiveProfiles(value = "mariadb")
+class SpringMariaDbTest {
+
+	@Autowired
+	DataSource ds;
+
+	// @Test
+	void testMariaDbSchema() throws Exception {
+		assertTrue(true);
+		try (Connection conn = ds.getConnection();
+				final PreparedStatement stmt = conn.prepareStatement("select * from vnf_package", 0)) {
+			stmt.execute();
+		}
+	}
+}
