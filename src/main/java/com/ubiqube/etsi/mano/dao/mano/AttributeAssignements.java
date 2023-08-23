57f1c516c13ee1013bac42fb2134648903b8053a
Olivier Vignaud <ovi@ubiqube.com>
2023-08-23 15:20:55 +0200

    Add name in AttributeAssignements.

--- src/main/java/com/ubiqube/etsi/mano/dao/mano/AttributeAssignements.java
+++ src/main/java/com/ubiqube/etsi/mano/dao/mano/AttributeAssignements.java
@@ -12,7 +12,7 @@
  *     GNU General Public License for more details.
  *
  *     You should have received a copy of the GNU General Public License
- *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *     along with this program.  If not, see https://www.gnu.org/licenses/.
  */
 package com.ubiqube.etsi.mano.dao.mano;
 
@@ -45,6 +45,8 @@ public class AttributeAssignements implements Serializable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private UUID id;
+
+	private String name;
 
 	@ElementCollection(fetch = FetchType.EAGER)
 	private List<String> value;
