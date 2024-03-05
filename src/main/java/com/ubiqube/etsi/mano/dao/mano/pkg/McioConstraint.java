1c9e729b41e4201c4038eba6434a7ea6df14db12
Olivier Vignaud <ovi@ubiqube.com>
2024-03-05 12:41:01 +0100

    Update license headers.

--- src/main/java/com/ubiqube/etsi/mano/dao/mano/pkg/McioConstraint.java
+++ src/main/java/com/ubiqube/etsi/mano/dao/mano/pkg/McioConstraint.java
@@ -1,5 +1,5 @@
 /**
- *     Copyright (C) 2019-2023 Ubiqube.
+ *     Copyright (C) 2019-2024 Ubiqube.
  *
  *     This program is free software: you can redistribute it and/or modify
  *     it under the terms of the GNU General Public License as published by
