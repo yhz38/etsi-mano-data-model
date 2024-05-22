172366a31dd876e1733333ef44e86349912a7fc4
Olivier Vignaud <ovi@ubiqube.com>
2024-05-22 20:50:37 +0200

    ✨ Add CAPI server connection informations.

---
+++ src/main/java/com/ubiqube/etsi/mano/dao/mano/cnf/capi/CapiServer.java
@@ -0,0 +1,55 @@
+/**
+ *     Copyright (C) 2019-2024 Ubiqube.
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package com.ubiqube.etsi.mano.dao.mano.cnf.capi;
+
+import java.util.UUID;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class CapiServer {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private UUID id;
+
+	private String name;
+
+	@Column(length = 5000)
+	@JsonProperty("certificate-authority-data")
+	private String certificateAuthorityData;
+
+	private String url;
+
+	@Column(length = 5000)
+	@JsonProperty("client-certificate-data")
+	private String clientCertificateData;
+
+	@Column(length = 5000)
+	@JsonProperty("client-key-data")
+	private String clientKeyData;
+}
